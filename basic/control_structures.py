4a53385460deb5ee16f57ff3a06a2a270dd410e7
Shintanuies <gomesvidalbrunorosa@gmail.com>
2025-06-17 18:37:02 -0300

    add control_structures.py arch

---
+++ basic/control_structures.py
@@ -0,0 +1,12 @@
+age = 20
+
+if age < 18:
+    print('He is underage')
+elif age >= 18 and age > 60:
+    print('Is of legal age')
+elif age > 15:
+    print('Greater than 15 years')
+elif age == 60:
+    print('Congratulations on your 60th birthday!')
+else:
+    print('You are an elderly person')
