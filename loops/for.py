d47c9d89257041ad1186a26a7be176bce86d4218
Shintanuies <gomesvidalbrunorosa@gmail.com>
2025-06-18 01:23:01 -0300

    add exercicies in arch for.py and while.py

--- loops/for.py
+++ loops/for.py
@@ -1,4 +1,9 @@
+print('imprime o array de frutas')
 frutas = ['maça', 'banana', 'laranja']
 
 for fruta in frutas:
     print(fruta)
+
+print('\nimprime os numeros de 1 a 5 multiplicados por 2')
+for numero in range(1, 6):
+    print(numero * 2)
--- loops/while.py
+++ loops/while.py
@@ -1,5 +1,13 @@
+print('Imprime contador de 0 a 4 usando o laço while')
 contador = 0
 
 while contador < 5:
     print(contador)
     contador += 1
+
+print('imprime o contador2 multiplicado por 2 enquanto o mesmo for menor ou igual a 5 usando o laço while')
+contador2 = 1
+
+while contador2 <= 5:
+    print(contador2 * 2)
+    contador2 += 1
